9fb34e89624e8dc8398aecc07364b34732b2a11c
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-02-12 11:11:02 +0000

    added activity to display payment

--- app/src/main/java/com/example/govpay/DisplayPayment.java
+++ app/src/main/java/com/example/govpay/DisplayPayment.java
@@ -1,12 +1,9 @@
 package com.example.govpay;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -15,41 +12,32 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class WebPage extends AppCompatActivity {
-
-    private String json_object;
+public class DisplayPayment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent intent = getIntent();
-        Long amount = Long.parseLong(intent.getStringExtra("AMOUNT"));
-        System.out.println(amount);
-
-        // Create payment object
-        Payment payment = new Payment(amount);
-        // Converts payment to JSON
-        Gson gson = new Gson();
-        String json = gson.toJson(payment);
-        json_object = json;
-
         super.onCreate(savedInstanceState);
-        apiCall();
+        setContentView(R.layout.activity_display_payment);
+
+        SharedPreferences sharedPref = DisplayPayment.this.getPreferences(Context.MODE_PRIVATE);
+        String payment_id = sharedPref.getString("payment_id", "no payment id");
+        System.out.println(payment_id);
+        apiCall(payment_id);
+
     }
 
-    public void apiCall() {
+    public void apiCall(String payment_id) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://publicapi.payments.service.gov.uk/v1/payments";
+        String url = "https://publicapi.payments.service.gov.uk/v1/payments/" + payment_id;
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -57,11 +45,14 @@ public class WebPage extends AppCompatActivity {
                         System.out.println(response);
                         try {
                             JSONObject item = new JSONObject(response);
+                            /*
+
                             String url = item.getJSONObject("_links").getJSONObject("next_url").getString("href");
                             String payment_id = item.getString("payment_id");
                             System.out.println(payment_id);
-                            store_payment_id(payment_id);
-                            setUpWeb(url);
+                            setUpViews();
+
+                            */
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -84,36 +75,10 @@ public class WebPage extends AppCompatActivity {
                 return headers;
             }
 
-            @Override
-            public String getBodyContentType(){
-                return "application/json; charset=utf-8";
-            }
-
-            @Override
-            public byte[] getBody() throws AuthFailureError{
-                try {
-                    return json_object.getBytes("utf-8"); // For DATA put our string
-                } catch (Exception e) {
-                    return null;
-                }
-            }
-
         };
         queue.add(stringRequest);
     }
+    public void setUpViews() {
 
-    public void setUpWeb(String url){
-        WebView myWebView = new WebView(this);
-        myWebView.setWebViewClient(new WebViewClient());
-        setContentView(myWebView);
-        myWebView.loadUrl(url);
-    }
-
-    public void store_payment_id(String payment_id) {
-        SharedPreferences sharedPref = WebPage.this.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("payment_id", payment_id);
-        editor.commit();
-        System.out.println("Stored Payment ID");
     }
 }
